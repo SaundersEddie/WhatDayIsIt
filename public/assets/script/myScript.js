47fd70ad9e494696b281a55339c840c4e5dab822
Eddie Saunders <saunders.eddie@outlook.com>
2020-10-18 12:46:31 -0400

    Tidied up HTML and page appearance

--- public/assets/script/myScript.js
+++ public/assets/script/myScript.js
@@ -3,11 +3,6 @@
 // As we're not worried about year, just date and month this should be 
 // fairly simple to implement using the date functionality
 
-// const nodemon = require("nodemon");
-
-// myMonthTable will be used to create the calendar to display. Each day
-// will be a button
-
 const leapYear = (year) => { return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0) };
 
 let myMonths = [['January',31],['February',0],['March',31],['April',30],['May',31],['June',30],['July',31],['August',31],['September',30],['October',31],['November',30],['December',31]]
@@ -20,25 +15,25 @@ leapYear(new Date().getFullYear()) ? myMonths[1] = ['February',29] :  myMonths[1
 const displayOneMonth = (monthNumber) => {
    let myTableStart = "<table class='center'><tr>"
    let myTableEnd = "</tr></table>" 
-   let myTestMonthTable = "";
+   let myMonthTable = "";
    let myDayNumber = 0;
    for (let i=0; i < myMonths[monthNumber][1]; i++) {
       let myCellID = myMonths[monthNumber][0].slice(0,3) + (i+1);
-      myTestMonthTable += `<td> <button class='buttonCustom' id='${myCellID}'> ${i+1} </button> </td>`;
+      myMonthTable += `<td> <button class='buttonCustom' id='${myCellID}'> ${i+1} </button> </td>`;
       if (myDayNumber < 6 ) {
          myDayNumber++;
       } else {
-         myTestMonthTable += "</tr><tr>";
+         myMonthTable += "</tr><tr>";
          myDayNumber = 0;
       }
    }
-   let myTestTable = myTableStart + myTestMonthTable + myTableEnd;
+   let myTable = myTableStart + myMonthTable + myTableEnd;
    $('#myCalendarHere').empty();
-   $('#myCalendarHere').append(myTestTable)
+   $('#myCalendarHere').append(myTable)
 }
 
 const monthTable = (monthNumber) => {
-   $('#monthName').text(myMonths[monthNumber][0])
+   $('#monthName').text(`${myMonths[monthNumber][0]}`)
    displayOneMonth(monthNumber);
 }
 
@@ -46,7 +41,6 @@ monthTable (myMonthNumber);
 
 $(document).on('click','button', function() {
    var id = $(this).attr('id')
-   console.log (id)
    switch (id) {
       case "lastMonth":
          monthToDisplay <= 0 ? monthToDisplay=11: monthToDisplay--;
@@ -57,6 +51,7 @@ $(document).on('click','button', function() {
          monthTable(monthToDisplay);
          break;
       default:
+         // This is where our call to the database will take place
          console.log ("Not implemented");
   }
    return false; 
