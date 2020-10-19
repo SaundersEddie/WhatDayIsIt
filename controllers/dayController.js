a1c95d41918ec239467e4a342f954375f7d46bd3
Eddie Saunders <saunders.eddie@outlook.com>
2020-10-19 10:05:32 -0400

    Added in list all route, confirmed working in browser

--- controllers/dayController.js
+++ controllers/dayController.js
@@ -1,25 +1,24 @@
-// controller for our days code
+// controller for our days info, this should probably be changed to a far simpler name
+// such as routes and follow best practice/
 
-// const express=require("express");
 const router = require ("express").Router();
-
 const MyDayInfo = require ("../schema/days_schema");
 
-console.log ("In days_controller");
+//  console.log ("In days_controller");
 
-// router.get ('/:id', (req,res) => {
-//     console.log ("Here");
-//     let condition = 'id =' + req.params.id;
-    
-// })
+// List all dates
+router.route ("/").get ((req, res) => {
+    MyDayInfo.find()
+        .then((daysInfo) => res.json(daysInfo))
+        .catch((err) => res.status(400).json("Error: "+err));
+});
 
+// Add new date
 router.route("/add").post((req, res) => {
-    console.log ("my Req: ", req.body);
+    // console.log ("my Req: ", req.body);
     const day = req.body.day;
     const name = req.body.name;
-
     const newDay = new MyDayInfo ({day, name});
-
     newDay 
         .save()
         .then(() => res.json("Date Added"))
