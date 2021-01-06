a0a436256213829bf8a838113d3aa56a8a289aad
Eddie Saunders <saunders.eddie@outlook.com>
2021-01-06 04:25:49 -0500

    Code update to not start server until database is connected.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function () {
+export default function Header () {
     return (
         <div>
             <h1>Our Header</h1>
--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-// Server code
-// Eddie Saunders saunders.eddie@outlook.com 13th May 2020
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import express from 'express';
@@ -9,24 +7,24 @@ const PORT = process.env.PORT || 5000;
 const MONGO_CONNECT = process.env.MONGO_DB;
 const app = express();
 
-
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(MONGO_CONNECT, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-
 // Open Connections 
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(
+          "/static",
+          express.static(path.join(__dirname, "../client/build/static"))
+  );
+  app.get("/", (req, res) => {
+          res.sendFile(path.join(__dirname, "../client/build/"));
+  });
+}
 
-app.listen(PORT, () => {
-  console.log("App listening on PORT " + PORT);
-});
+mongoose.connect(MONGO_CONNECT, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`API Server listening on ${PORT}`)))
+  .catch((error) => console.log("An Error Occurred: ", error.message));
+
+// Setup Routes
