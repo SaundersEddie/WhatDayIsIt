7e16d773e202317311eba198a42b202bdab1b53d
Eddie Saunders <saunders.eddie@outlook.com>
2021-01-12 12:28:31 -0500

    Adding packages for Heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import ourDays from './routes/dayRoutes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +17,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 // Open Connections 
 
